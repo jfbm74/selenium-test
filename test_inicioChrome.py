730cfff5025ad50c9b9b8d1cb8f3675241d61b0d
jfbm74 <jfbm74@gmail.com>
2021-03-02 12:37:45 -0500

    sync repo

--- test_inicioChrome.py
+++ test_inicioChrome.py
@@ -10,6 +10,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+username = os.environ["SAUCE_USERNAME"]
+access_key = os.environ["SAUCE_ACCESS_KEY"]
+capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+hub_url = "%s:%s@localhost:4445" % (username, access_key)
+driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
+
 
 class TestInicio():
   def setup_method(self, method):
